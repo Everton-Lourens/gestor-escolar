d85b4f4b1adf546f9d1f013094e1cf0b2005710b
Everton Lourens <evertonlour98@gmail.com>
2024-03-09 19:06:24 -0300

    COlocando o token de acesso para expirar em 10 anos

--- backend/src/config/auth.ts
+++ backend/src/config/auth.ts
@@ -1,7 +1,21 @@
+/*
+// TOKEN DEFAULT
+
 export default {
   secretToken: 'a97adc1b32357d9481f6d73fba9ef2c7',
   secretRefreshToken: '60de3ce66da5bff63a0c4b245c15b8c8',
   expiresInToken: '55m',
   expiresInRefreshToken: '30d',
+  expiresRefreshTokenDays: 30,
+}
+*/
+
+// NEW TOKEN
+
+export default {
+  secretToken: 'a97adc1b32357d9481f6d73fba9ef2c7',
+  secretRefreshToken: '60de3ce66da5bff63a0c4b245c15b8c8',
+  expiresInToken: '3650d', // Tempo de expiração de 10 anos (3650 dias)
+  expiresInRefreshToken: '3650d', // Tempo de expiração do refresh token de 10 anos (3650 dias)
   expiresRefreshTokenDays: 30,
 }
